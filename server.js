ae3894a5fb5ca0a5c124383730f94de93e6df18b
Kristina <kristina.dudnyk20@gmail.com>
2023-07-24 20:02:51 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const PORT = 3001;
+const cors = require("cors");
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const messages = [
+  {
+    id: 0,
+    from: "Bart",
+    text: "Welcome to CYF chat system!",
+  },
+];
+
+//This array is our "data store".
+
+app.get("/", function (req, res) {
+  console.log("Server is up");
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/messages", (req, res) => {
+  console.log();
+  // res.status(200).send(welcomeMessage);
+  res.send(messages);
+});
+
+app.post("/messages", (req, res) => {
+  // console.log(req.body)
+  const { from, text } = req.body;
+
+  if (!from) {
+    res.status(400).send("You missing your name");
+  } else if (!text) {
+    res.status(404).send("You missing your message");
+  } else if (!from && !text) {
+    res.status(400).send("Your missing everything");
+  } else if (from && text) {
+    messages.push({
+      id: messages.length + 1,
+      from: from,
+      text: text,
+    });
+    res.status(201).redirect("/");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on Port ${PORT}`);
+});
